aa9fc728334f879f24b2f717214ed546d8132b80
Nicolas Diaz <nikodiaz56@gmail.com>
2024-09-02 18:55:30 -0300

    feat: add title to home section

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <section id="home" className="max-w-md w-full space-y-8 mb-12">
+      <div>
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Shorten your URL
+        </h2>
+        <p className="text-center text-sm text-gray-500">Enter a long URL to get a short link</p>
+      </div>
+    </section>
+  )
+}
+
+export default Home
